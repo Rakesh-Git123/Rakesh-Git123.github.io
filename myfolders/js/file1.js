516861f7608e9eee2fa3d7a0bba9be111194c0b8
Rakesh-Git123 <rakeshgit1234@gmail.com>
2025-06-20 10:01:27 +0530

    updated

---
+++ myfolders/js/file1.js
@@ -0,0 +1,18 @@
+let name="John";
+let age=21;
+let promoted=true;
+let score
+let city=null
+// console.log(name)
+// console.log(typeof name);
+// console.log(typeof score);
+// console.log(typeof city); //object
+// let strAge=String(age)
+// console.log(typeof strAge);
+// let rating="5"
+// console.log(typeof rating);
+// let numRating=Number(rating);
+// console.log(typeof numRating);
+// let flag="true"
+// let bFlag=Boolean(flag)
+// console.log(typeof bFlag);

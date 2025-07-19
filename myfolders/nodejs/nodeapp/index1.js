57bb5133c7bcb1715034da49804eee9806f6ec83
Rakesh-Git123 <rakeshgit1234@gmail.com>
2025-07-19 20:07:43 +0530

    updated

--- myfolders/nodejs/nodeapp/index1.js
+++ myfolders/nodejs/nodeapp/index1.js
@@ -8,7 +8,12 @@ import fs from "fs"
 //     if(err) throw err;
 // })
 
-fs.readFile("about.txt",'utf-8',(err,data)=>{
-    if(err) throw err;
-    console.log(data);
-})
+// fs.readFile("about.txt",'utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+// fs.rename("myfile.txt","newfile.txt",(err)=>{
+//     if(err) throw err;
+// })
+
